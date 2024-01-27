c28ff91feba436623fbce607aab86246b28f04ff
nima1399 <hekmatinima@gmail.com>
2024-01-27 21:44:41 +0330

    some improvements for config

--- gigit.c
+++ gigit.c
@@ -5,8 +5,9 @@
 #include <sys/types.h>
 #include <stdbool.h>
 
-#define INVALID printf("Invalid command\n");
+#define test printf("test\n");
 
+int config(int argc, char *argv[]);
 int init();
 int gigitExists(char *path);
 
@@ -14,12 +15,18 @@ int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        INVALID
+        printf("Invalid command\n");
         return 1;
     }
     if (strcmp(argv[1], "init") == 0)
     {
         if (init())
+            return 1;
+    }
+    if (strcmp(argv[1], "config") == 0)
+    {
+        printf("config\n");
+        if (config(argc, argv))
             return 1;
     }
     printf("hello world\n");
@@ -38,9 +45,13 @@ int init()
     if (gigitExists(path))
     {
         printf("Already a gigit repository\n");
+        closedir(dir);
         return 1;
     }
     mkdir(".gigit");
+    char *linkPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\projects path.txt";
+    FILE *projectLink = fopen(linkPath, "a");
+    fprintf(projectLink, "%s\n", path);
     closedir(dir);
     return 0;
 }
@@ -48,20 +59,85 @@ int init()
 int gigitExists(char *path)
 {
     DIR *dir = opendir(path);
+    if (!dir)
+    {
+        return 0;
+    }
+
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL)
     {
         if (strcmp(entry->d_name, ".gigit") == 0)
         {
+            closedir(dir);
             return 1;
         }
     }
+
     closedir(dir);
-    if (!strcmp(path, "C:"))
+
+    if (strcmp(path, ".") == 0)
     {
         return 0;
     }
+
     char *str = strrchr(path, '\\');
-    str[0] = '\0';
-    return gigitExists(path);
+    if (str != NULL)
+    {
+        str[0] = '\0';
+        return gigitExists(path);
+    }
+
+    return 0;
+}
+
+int config(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        printf("Invalid command\n");
+        return 1;
+    }
+
+    bool global = false;
+
+    if (strcmp(argv[2], "-global") == 0)
+    {
+        global = true;
+    }
+    if (strcmp(argv[3], "user.name") == 0 && argc == 5)
+    {
+        FILE *file = fopen("gigit\\config\\username.txt", "w");
+
+        if (!file)
+        {
+            perror("Error opening .gigit\\config file");
+            return 1;
+        }
+
+        fprintf(file, "%s", argv[4]);
+        fclose(file);
+    }
+    else if (strcmp(argv[3], "user.email") == 0 && argc == 5)
+    {
+        FILE *file = fopen("gigit\\config\\useremail.txt", "w");
+
+        if (!file)
+        {
+            perror("Error opening .gigit\\config file");
+            return 1;
+        }
+
+        fprintf(file, "%s", argv[4]);
+        fclose(file);
+    }
+    else
+    {
+        printf("Invalid command\n");
+        return 1;
+    }
+    if (global)
+    {
+        // complete here
+    }
 }
