df0077c1c4b6c7f85702897468a32ecb04cd9888
nima1399 <hekmatinima@gmail.com>
2024-01-28 14:41:27 +0330

    completed config

--- gigit.c
+++ gigit.c
@@ -4,12 +4,14 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define test printf("test\n");
 
 int config(int argc, char *argv[]);
 int init();
 int gigitExists(char *path);
+int alias(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
@@ -48,13 +50,19 @@ int init()
         closedir(dir);
         return 1;
     }
+    char *currentPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\currentProjectPath.txt";
+    char *linkPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\projectsPath.txt";
+    if (fopen(currentPath, "r") == NULL)
+    {
+        printf("enter username and email using gigit config before init\n");
+        return 1;
+    }
+
     mkdir(".gigit");
     strcat(path, "\\.gigit");
-    char *linkPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\projectsPath.txt";
     FILE *projectLink = fopen(linkPath, "a");
     fprintf(projectLink, "%s\n", path);
     fclose(projectLink);
-    char *currentPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\currentProjectPath.txt";
     FILE *currentProjectLink = fopen(currentPath, "w");
     fprintf(currentProjectLink, "%s\n", path);
     fclose(currentProjectLink);
@@ -114,37 +122,43 @@ int config(int argc, char *argv[])
         printf("Invalid command\n");
         return 1;
     }
-
+    int user = 2;
     bool global = false;
 
     if (strcmp(argv[2], "-global") == 0)
     {
         global = true;
+        user = 3;
     }
-    if (strcmp(argv[3], "user.name") == 0 && argc == 5)
+    if (strncmp(argv[user], "alias.", 6) == 0)
+    {
+        alias(argc, argv);
+        return 0;
+    }
+    if (strcmp(argv[user], "user.name") == 0 && argc == user + 2)
     {
         FILE *file = fopen("gigit\\config\\username.txt", "w");
 
         if (!file)
         {
-            perror("Error opening .gigit\\config file");
+            printf("Error opening gigit\\config file");
             return 1;
         }
 
-        fprintf(file, "%s", argv[4]);
+        fprintf(file, "%s", argv[user + 1]);
         fclose(file);
     }
-    else if (strcmp(argv[3], "user.email") == 0 && argc == 5)
+    else if (strcmp(argv[user], "user.email") == 0 && argc == user + 2)
     {
         FILE *file = fopen("gigit\\config\\useremail.txt", "w");
 
         if (!file)
         {
-            perror("Error opening .gigit\\config file");
+            printf("Error opening gigit\\config file");
             return 1;
         }
 
-        fprintf(file, "%s", argv[4]);
+        fprintf(file, "%s", argv[user + 1]);
         fclose(file);
     }
     else
@@ -155,10 +169,117 @@ int config(int argc, char *argv[])
     if (global)
     {
         char *linkPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\projectsPath.txt";
-        
+        FILE *projectsLink = fopen(linkPath, "r");
+        char *path = NULL;
+        char *read;
+        while ((read = fgets(path, 200, projectsLink)) != NULL)
+        {
+            path[strlen(path) - 1] = '\0';
+            if (!strcmp(argv[user], "user.name"))
+            {
+                char *usernamePath = strcat(path, "\\.gigit\\config\\username.txt");
+                FILE *usernameFile = fopen(usernamePath, "w");
+                fprintf(usernameFile, "%s\n", argv[user + 1]);
+                fclose(usernameFile);
+            }
+            else if (!strcmp(argv[user], "user.email"))
+            {
+                char *useremailPath = strcat(path, "\\.gigit\\config\\useremail.txt");
+                FILE *useremailFile = fopen(useremailPath, "w");
+                fprintf(useremailFile, "%s\n", argv[user + 1]);
+                fclose(useremailFile);
+            }
+        }
     }
     else
     {
-        // complete here
+        char *currentPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\currentProjectPath.txt";
+        FILE *currentProjectLink = fopen(currentPath, "r");
+        if (!currentProjectLink)
+        {
+            printf("Error opening currentProjectPath.txt\n");
+            return 1;
+        }
+        char *path = NULL;
+        char *read;
+        while ((read = fgets(path, 200, currentProjectLink)) != NULL)
+        {
+            path[strlen(path) - 1] = '\0';
+            if (!strcmp(argv[user], "user.name"))
+            {
+                char *usernamePath = strcat(path, "\\.gigit\\config\\username.txt");
+                FILE *usernameFile = fopen(usernamePath, "w");
+                fprintf(usernameFile, "%s\n", argv[user + 1]);
+                fclose(usernameFile);
+            }
+            else if (!strcmp(argv[user], "user.email"))
+            {
+                char *useremailPath = strcat(path, "\\.gigit\\config\\useremail.txt");
+                FILE *useremailFile = fopen(useremailPath, "w");
+                fprintf(useremailFile, "%s\n", argv[user + 1]);
+                fclose(useremailFile);
+            }
+        }
     }
+}
+
+int alias(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        printf("Invalid command\n");
+        return 1;
+    }
+    int user = 2;
+    bool global = false;
+
+    if (strcmp(argv[2], "-global") == 0)
+    {
+        global = true;
+        user = 3;
+    }
+    if (strcmp(argv[user], "alias.") == 0 && argc == user + 2)
+    {
+        char *linkPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\alias.txt";
+        FILE *projectsLink = fopen(linkPath, "r");
+        char *path = NULL;
+        char *read;
+        while ((read = fgets(path, 200, projectsLink)) != NULL)
+        {
+            path[strlen(path) - 1] = '\0';
+            char *aliasPath = strcat(path, "\\.gigit\\config\\alias.txt");
+            FILE *aliasFile = fopen(aliasPath, "a");
+            fprintf(aliasFile, "%s %s\n", argv[user + 1], argv[user + 2]);
+            fclose(aliasFile);
+        }
+    }
+    else
+    {
+        printf("Invalid command\n");
+        return 1;
+    }
+    if (global)
+    {
+        char *linkPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\projectsPath.txt";
+        FILE *projectsLink = fopen(linkPath, "r");
+        char *path = NULL;
+        char *read;
+        while ((read = fgets(path, 200, projectsLink)) != NULL)
+        {
+            path[strlen(path) - 1] = '\0';
+            char *aliasPath = strcat(path, "\\.gigit\\config\\alias.txt");
+            FILE *aliasFile = fopen(aliasPath, "a");
+            fprintf(aliasFile, "%s %s\n", argv[user + 1], argv[user + 2]);
+            fclose(aliasFile);
+        }
+    }
+    else
+    {
+        char *currentPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\currentProjectPath.txt";
+        FILE *currentProjectLink = fopen(currentPath, "r");
+        if (!currentProjectLink)
+        {
+            printf("Error opening currentProjectPath.txt\n");
+            return
+        }
 }
