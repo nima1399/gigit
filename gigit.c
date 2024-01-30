9c5e22db3e671d8ec40b13c5bc901b94e16c581d
nima1399 <hekmatinima@gmail.com>
2024-01-30 14:01:30 +0330

    improvements

--- gigit.c
+++ gigit.c
@@ -3,8 +3,10 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ftw.h>
 
 #define test printf("test\n");
 
@@ -13,6 +15,9 @@ int init();
 char *gigitExists(char *path);
 int alias(int argc, char *argv[]);
 int aliasCheck(int argc, char *argv[]);
+int add(int argc, char *argv[]);
+int mkdirRecursive(char *path);
+int letterCount(char *str, char token);
 
 int main(int argc, char *argv[])
 {
@@ -30,6 +35,11 @@ int main(int argc, char *argv[])
     {
         printf("config\n");
         if (config(argc, argv))
+            return 1;
+    }
+    else if (strcmp(argv[1], "add") == 0)
+    {
+        if (add(argc, argv))
             return 1;
     }
     else 
@@ -58,12 +68,18 @@ int init()
     }
     char *currentPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\currentProjectPath.txt";
     char *linkPath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\projectsPath.txt";
-    if (fopen(currentPath, "r") == NULL)
+    char *usernamepath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\username.txt";
+    char *useremailpath = "C:\\Users\\Admin\\OneDrive\\Desktop\\SUT\\gigit\\gigit\\config\\useremail.txt";
+    if (fopen(usernamepath, "r") == NULL)
     {
         printf("enter username and email using gigit config before init\n");
         return 1;
     }
-
+    if (fopen(useremailpath, "r") == NULL)
+    {
+        printf("enter username and email using gigit config before init\n");
+        return 1;
+    }
     mkdir(".gigit");
     strcat(path, "\\.gigit");
     FILE *projectLink = fopen(linkPath, "a");
@@ -76,11 +92,19 @@ int init()
     mkdir(configPath);
     char *usernamePath = ".gigit\\config\\username.txt";
     FILE *usernameFile = fopen(usernamePath, "w");
-    fprintf(usernameFile, "%s\n", "username");
+    char username[100];
+    FILE *usernameFile2 = fopen(usernamepath, "r");
+    fgets(username, 100, usernameFile2);
+    username[strlen(username)] = '\0';
+    fprintf(usernameFile, "%s\n", username);
     fclose(usernameFile);
     char *useremailPath = ".gigit\\config\\useremail.txt";
     FILE *useremailFile = fopen(useremailPath, "w");
-    fprintf(useremailFile, "%s\n", "useremail");
+    char useremail[100];
+    FILE *useremailFile2 = fopen(useremailpath, "r");
+    fgets(useremail, 100, useremailFile2);
+    useremail[strlen(useremail)] = '\0';
+    fprintf(useremailFile, "%s\n", useremail);
     fclose(useremailFile);
     closedir(dir);
     printf("Initialized empty gigit repository in %s\n", path);
@@ -107,7 +131,7 @@ char *gigitExists(char *path)
 
     closedir(dir);
 
-    if (strcmp(path, ".") == 0)
+    if (strcmp(path, "C:") == 0)
     {
         return NULL;
     }
@@ -300,6 +324,117 @@ int aliasCheck(int argc, char *argv[])
             return 0;
         }
     }
-    printf("alias not found\n");
+    printf("invalid input or alias not found\n");
     return 1;
+}
+
+int add(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        printf("Invalid command\n");
+        return 1;
+    }
+    if (argc == 3)
+    {
+        struct stat s;
+        if (stat(argv[2], &s) == 0)
+        {
+            if (S_ISDIR(s.st_mode))
+            {
+                DIR *dir = opendir(argv[2]);
+                if (!dir)
+                {
+                    printf("Error: not a directory\n");
+                    return 1;
+                }
+                char *path = dir->dd_name;
+                char *gigitpath = gigitExists(path);
+                if (gigitpath == NULL)
+                {
+                    printf("Not a gigit repository\n");
+                    closedir(dir);
+                    return 1;
+                }
+                char *relativePath = path + strlen(gigitpath) - strlen("\\.gigit");
+                char *savePath = strcat(gigitpath, "\\stage");
+                savePath = strcat(savePath, relativePath);
+                mkdirRecursive(savePath);
+                struct dirent *entry;
+                while ((entry = readdir(dir)) != NULL)
+                {
+                    if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+                    {
+                        char *newPath = strcat(path, "\\");
+                        newPath = strcat(newPath, entry->d_name);
+                        add(4, (char *[]){"add", argv[2], newPath});
+                    }
+                }
+
+            }
+            else
+            {
+                // argv[2] is not a directory
+                char *gigitpath = gigitExists(argv[2]);
+                if (gigitpath == NULL)
+                {
+                    printf("Not a gigit repository\n");
+                    return 1;
+                }
+                char *relativePath = argv[2] + strlen(gigitpath) - strlen("\\.gigit");
+                char *savePath = strcat(gigitpath, "\\stage");  
+                savePath = strcat(savePath, relativePath);
+                char *removeFileFromPath = strrchr(savePath, '\\');
+                removeFileFromPath[0] = '\0';
+                mkdirRecursive(savePath);
+                savePath = strcat(savePath, "\\stage");
+                savePath = strcat(savePath, relativePath);
+                if (access(savePath, F_OK) == 0)
+                {
+                    return 1;
+                }
+                FILE *file = fopen(savePath, "w");
+                FILE *file2 = fopen(argv[2], "r");
+                char c;
+                while ((c = fgetc(file2)) != EOF)
+                {
+                    fputc(c, file);
+                }
+            }
+        }
+        else
+        {
+            // error
+            printf("directory or file doesn't exists\n");
+            return 1;
+        }
+        return 0;
+    }
+}
+
+int letterCount(char *str, char token)
+{
+    int count = 0;
+    while (str[0] != '\0')
+    {
+        if (str[0] == token)
+            count++;
+        str++;
+    }
+    return count;
+}
+
+int mkdirRecursive(char *path)
+{
+    if (!strcmp(path, "C:"))
+    {
+        return 0;
+    }
+    char *path2;
+    strcpy(path2, path);
+    char *token = strrchr(path2, '\\');
+    token[0] = '\0';
+    mkdirRecursive(path2);
+    mkdir(path);
+    return 0;
 }
